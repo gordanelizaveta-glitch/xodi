f77326ae3225158d7ecec7d8118ec6b9718409f2
gordanelizaveta-glitch <gordanelizaveta@gmail.com>
2026-02-05 10:48:15 +0000

    3

--- js/game.js
+++ js/game.js
@@ -219,8 +219,8 @@ backgroundColor: 'rgba(0,0,0,0)',
     autoCenter: Phaser.Scale.CENTER_BOTH,
 
     // базовый размер, дальше Phaser сам ресайзит
-    width: 720,
-    height: 1280
+    width: 1920,
+    height: 1080
   },
   scene: []
 };
